a3b45fcd443ad768978e555fea6d2da7c037aa55
Dario Alcivar <dario_alcivar13@hotmail.com>
2023-07-27 16:03:52 -0500

    Agregar archivos de proyecto.

---
+++ TeamLID.TravelExperts.App/Models/PackagesModel.cs
@@ -0,0 +1,34 @@
+﻿/*
+Author: Ibraheem
+Purpose: Get packages
+*/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace TeamLID.TravelExperts.App.Models
+{
+    /// <summary>
+    /// Packages model.
+    /// </summary>
+    public class PackagesModel
+    {
+        public int PackageId { get; set; }
+
+        [DisplayName("Titulo")]
+        public string PkgName { get; set; }
+
+        [DataType(DataType.Date), DisplayName("Fecha Inicio")]
+        public DateTime? PkgStartDate { get; set; }
+
+        [DataType(DataType.Date), DisplayName("Fecha Fin")]
+        public DateTime? PkgEndDate { get; set; }
+
+        [DisplayName("Descripcion")]
+        public string PkgDesc { get; set; }
+
+        [DisplayName("Precio (US$)")]
+        public string PkgBasePrice { get; set; }
+    }
+}
---
+++ TeamLID.TravelExperts.App/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using TeamLID.TravelExperts.Repository.Domain;
+
+namespace TeamLID.TravelExperts.App
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+
+    internal class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+            var connection = @"Server=localhost\sqlexpress;Database=TravelExperts;Trusted_Connection=True;ConnectRetryCount=0";
+
+
+            services.AddDbContext<TravelExpertsContext>(options => options.UseSqlServer(connection));
+            services.AddSession(options => { options.IdleTimeout = TimeSpan.FromMinutes(30); });
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+            app.UseSession();
+            app.UseRouting();
+            app.UseAuthorization();
+            app.UseHttpsRedirection();
+
+            /*app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });*/
+            app.UseEndpoints(endpoints => {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
